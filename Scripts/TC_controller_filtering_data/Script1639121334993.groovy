4841335ea787e5302f3d70e371a31fe956445e11
kazurayam <kazuaki.urayama@gmail.com>
2021-12-10 17:17:02 +0900

    the last sentense

--- Scripts/TC_controller_filtering_data/Script1639121334993.groovy
+++ Scripts/TC_controller_filtering_data/Script1639121334993.groovy
@@ -1,11 +1,8 @@
 import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
-
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
-
 import internal.GlobalVariable
 
 String pattern = GlobalVariable.DataStartsWithPattern
-
 println "pattern=${pattern}"
 
 File data = new File("./data.csv")
@@ -15,8 +12,6 @@ data.eachLine { line, lineNumber ->
 	if (items[1].toUpperCase().startsWith(pattern.toUpperCase())) {
 		WebUI.callTestCase(findTestCase("TC_worker"), ["seq": items[0], "data": items[1]])
 		count += 1
-	}
-	
+	}	
 }
-
 println "count=${count}"
